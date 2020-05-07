c077859805af0c122ea52022fe368413db5e1fea
Patrick Harl <therandomwoodworker@gmail.com>
2020-05-07 16:53:48 -0700

    project completed

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,59 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, act } from "@testing-library/react";
 import App from "./App";
+
+
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("can fill out form inputs correctly, submit, and verify submitted info", async () => {
+
+  const { getByLabelText, getByPlaceholderText, getByTestId, findByDisplayValue } = render(<App />)
+
+  const firstName = getByPlaceholderText(/edd/i)
+  const lastName = getByPlaceholderText(/burke/i)
+  const emailAddress = getByLabelText(/email*/i)
+  const message = getByLabelText(/message/i)
+
+  fireEvent.change(firstName, {target: {value:'Patrick'}})
+  fireEvent.change(lastName, {target: {value:'Harl'}})
+  fireEvent.change(emailAddress, {target: {value:'pat@gmail.com'}})
+  fireEvent.change(message, {target: {value:'Some message'}})
+
+  const submitBtn = getByTestId('submit-btn')
+  fireEvent.click(submitBtn)
+
+  const myFirstName = await findByDisplayValue(/patrick/i)
+  const myLastName = await findByDisplayValue(/harl/i)
+  const myEmail = await findByDisplayValue(/pat@gmail.com/i)
+  const myMessage = await findByDisplayValue(/some message/i)
+
+  expect(myFirstName).toBeInTheDocument()
+  expect(myLastName).toBeInTheDocument()
+  expect(myEmail).toBeInTheDocument()
+  expect(myMessage).toBeInTheDocument()
+  
+
+})
+
+test("validation is working", () => {
+
+
+  const { getByDisplayValue, getByPlaceholderText, getByLabelText } = render(<App />)
+
+  const firstName = getByPlaceholderText(/edd/i)
+  const lastName = getByPlaceholderText(/burke/i)
+  const emailAddress = getByLabelText(/email*/i)
+
+  act(() => firstName.focus())
+
+  expect(firstName).not.toHaveFocus()
+
+  
+})
+
+
+
+
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -18,10 +18,10 @@ const ContactForm = () => {
           <input
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            ref={register({ required: true })}
           />
           {errors.firstName && (
-            <p>Looks like there was an error: {errors.firstName.type}</p>
+            <p data-testid='firstname-err'>Looks like there was an error: {errors.firstName.type}</p>
           )}
         </div>
 
@@ -33,7 +33,7 @@ const ContactForm = () => {
             ref={register({ required: true })}
           />
           {errors.lastName && (
-            <p>Looks like there was an error: {errors.lastName.type}</p>
+            <p data-testid='lastname-err'>Looks like there was an error: {errors.lastName.type}</p>
           )}
         </div>
 
@@ -41,21 +41,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id="email" name="email" ref={register({ required: true })} />
           {errors.email && (
-            <p>Looks like there was an error: {errors.email.type}</p>
+            <p data-testid='email-err'>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea id="message" name="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid='submit-btn' type="submit" />
       </form>
     </div>
   );
